3b7775c94fc188cd86a6f00d934c3871d4a896d4
thabomakwea <thabomakwea@gmail.com>
2024-06-27 00:48:28 +0200

    Multiple Cors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
-# entrypoint.sh
+#!/bin/sh
 
-# Start the first instance on port 8081
+
+# Start the second instance on port 8081
 PORT=8081 node server.js &
 
-# Start the second instance on port 8082
+# Start the third instance on port 8082
 PORT=8082 node server.js &
 
 # Keep the container running
