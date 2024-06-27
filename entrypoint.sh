a25868182b2d99e03da93109a40f5141a0bc8f75
thabomakwea <thabomakwea@gmail.com>
2024-06-27 20:30:50 +0200

    Multiple Cors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-# Start the second instance on port 8081
+# Start the first instance on port 8081
 PORT=8081 node server.js &
 
-# Start the third instance on port 8082
+# Start the second instance on port 8082
 PORT=8082 node server.js &
+
+# Start the third instance on port 8080
+PORT=8083 node server.js &
+
+# Start the fourth instance on port 8080
+PORT=8084 node server.js &
+
+# Start the fifth instance on port 8080
+PORT=8085 node server.js &
+
 
 # Wait for all background processes to finish
 wait
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var port = process.env.PORT || 8080;
 // immediate abuse (e.g. denial of service). If you want to block all origins except for some,
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
-// var originWhitelist = ['https://www.radiotub.com', 'https://radiotub.com', 'https://radiotub.menanco.com'];
+// var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+var originWhitelist = ['https://www.radiotub.com', 'https://radiotub.com', 'https://radiotub.menanco.com', 'http://localhost:3000'];
 
 function parseEnvList(env) {
   if (!env) {
